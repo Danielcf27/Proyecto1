fb382fc6dc83e26c67d68bf5c4502e702a0f714b
DanielC <daniel14458@gmail.com>
2021-09-03 18:22:56 -0600

    Punto de control 2

--- Proyecto1/views.py
+++ Proyecto1/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 from django.template import loader
 def saludo(request):
-    t= loader.get_template('Proyecto1/uno.html')
+    t= loader.get_template('Proyecto1/indice.html')
     return 
